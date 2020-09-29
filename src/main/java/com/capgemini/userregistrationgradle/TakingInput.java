f9a6adf651a1a542d8b131a45cb60d5bf00dd5b7
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-29 23:32:21 +0530

    (Samiul Mamud) Added: Last name validation logic is added

--- src/main/java/com/capgemini/userregistrationgradle/TakingInput.java
+++ src/main/java/com/capgemini/userregistrationgradle/TakingInput.java
@@ -11,4 +11,11 @@ public class TakingInput {
 		firstName = scan.nextLine();
 		return firstName;
 	}
+
+	static String lastName() {
+		System.out.println("Enter Your last name");
+		String lastName;
+		lastName = scan.nextLine();
+		return lastName;
+	}
 }
