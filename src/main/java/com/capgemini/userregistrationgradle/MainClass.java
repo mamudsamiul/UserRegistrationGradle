edd30af388cb142624e1cb0b1b4aa8b9a15f04bb
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-30 00:49:22 +0530

    (Samiul Mamud) Refractored: Junit test case logic is added

--- src/main/java/com/capgemini/userregistrationgradle/MainClass.java
+++ src/main/java/com/capgemini/userregistrationgradle/MainClass.java
@@ -3,10 +3,5 @@ package com.capgemini.userregistrationgradle;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration");
-		CheckFormat.checkFirstName();
-		CheckFormat.checkLastName();
-		CheckFormat.checkEmail();
-		CheckFormat.checkPhone();
-		CheckFormat.checkPassword();
 	}
 }
